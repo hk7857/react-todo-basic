09780225182f5cc984805eb20166d4777bd96691
Hanran <hanrankim@gmail.com>
2018-03-29 12:14:08 -0700

    Completed display todo

---
+++ src/components/list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import listData from '../data/todo_items';
+
+console.log('ListData: ', listData);
+
+export default props => {
+
+    const listElements = listData.map((item, index) => {
+        return <li key={index} className="collection-item"> {item.title} </li>
+    });
+
+    return (
+        <ul className="collection">
+            {listElements}
+        </ul>
+    )
+}
---
+++ src/data/todo_items.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'Clean the house',
+        details: 'Clean the inside not the outside'
+    },
+    {
+        title: 'Air is dirty',
+        details: 'Need the clean air'
+    },
+    {
+        title: 'Floor is dirty',
+        details: 'Clean the floor'
+    },
+    {
+        title: 'Door is broken',
+        details: 'Fix the door'
+    },
+];
